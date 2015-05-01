c0730c0a5acf8f58ab0d804245402616418647bf
Ryan Roemer <ryan@loose-bits.com>
2015-05-01 08:54:00 -0700

    Add in missing file

---
+++ client/components/types-title.jsx
@@ -0,0 +1,16 @@
+/**
+ * Conversion types title.
+ */
+import React from "react";
+
+export default class Title extends React.Component {
+  render() {
+    return (
+      <span>
+        <span>to&nbsp;</span>
+        <span>...</span>
+        <span>&nbsp;!</span>
+      </span>
+    );
+  }
+}
