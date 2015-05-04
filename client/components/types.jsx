3c5ba09b60bbb6005c90e4d3282053cd8198b9ca
Ryan Roemer <ryan@loose-bits.com>
2015-05-03 18:52:49 -0700

    Fix nodemon, switch to mapped object for types.

--- client/components/types.jsx
+++ client/components/types.jsx
@@ -10,34 +10,46 @@ import Title from "./types-title";
 export default class Types extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: undefined };
+    this.state = { title: Types._TYPES.camel };
   }
 
-  _setType(type) {
-    this.setState({ title: type });
+  _setType(key) {
+    const title = Types._TYPES[key] ||
+      (key === Types._ALL ? Types._ALL_DESC : undefined);
+    this.setState({ title: title });
   }
 
   render() {
+    const types = Types._TYPES;
+    const items = Object.keys(types).map(key => (
+      <MenuItem key={key} onClick={this._setType.bind(this, key)}>
+        {types[key]}
+      </MenuItem>
+    ));
+
     return (
       <DropdownButton
         className="input-group-btn"
         pullRight
         title=<Title title={this.state.title} />
         >
-        <MenuItem onClick={this._setType.bind(this, "camel case")}>
-          camel case
-        </MenuItem>
-        <MenuItem onClick={this._setType.bind(this, "snake case")}>
-          snake case
-        </MenuItem>
-        <MenuItem onClick={this._setType.bind(this, "dasherized")}>
-          dasherized
-        </MenuItem>
+        {items}
         <MenuItem divider />
-        <MenuItem onClick={this._setType.bind(this, "all the things")}>
-          <strong>all the things</strong>
+        <MenuItem onClick={this._setType.bind(this, Types._ALL)}>
+          <strong>{Types._ALL_DESC}</strong>
         </MenuItem>
       </DropdownButton>
     );
   }
 }
+
+// All the individual types.
+Types._TYPES = {
+  camel: "camel case",
+  snake: "snake case",
+  dash: "dasherized"
+};
+
+// Special case "all types".
+Types._ALL = Object.keys(Types._TYPES);
+Types._ALL_DESC = "all the things";
